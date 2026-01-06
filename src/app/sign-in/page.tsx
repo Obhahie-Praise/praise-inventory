d5a143ee43ef32f70730808b708f856ae9100adc
Obhahie Praise <obhahiepraise@gmail.com>
2026-01-06 12:06:15 +0100

    removed the social sign in features due to unesscary bugs

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,32 +1,11 @@
 "use client";
 
-import { ArrowLeft, GithubIcon, LoaderCircle } from "lucide-react";
-import Image from "next/image";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import SignInClient from "./SignIn-client";
-import { signIn } from "@/lib/auth-client";
 import { useState } from "react";
 
 const SignInPage = () => {
-  
-    const [isGithubAuthenticating, setIsGithubAuthenticating] = useState(false);
-    const [isGoogleAuthenticating, setIsGoogleAuthenticating] = useState(false);
-  /* const signInSocial = async (provider: "github" | "google") => {
-    await signIn.social({
-      provider: provider,
-    });
-  };
- */
-  const socialSignIn = async (provider: string) => {
-    try {await signIn.social({
-      provider,
-      callbackURL: "/dashboard",
-    })} catch (error) {
-      setIsGithubAuthenticating(false);
-      setIsGoogleAuthenticating(false);
-    }
-    
-  };
   return (
     <div className="flex items-center justify-center min-h-screen">
       <Link
@@ -36,7 +15,7 @@ const SignInPage = () => {
         <ArrowLeft strokeWidth={1.3} />
       </Link>
       <div className="min-w-70">
-        <h1 className="text-3xl font-medium">Create new account</h1>
+        <h1 className="text-3xl font-medium text-center">Sign In</h1>
         <p className="text-zinc-400 my-1">
           Don't have an account?{" "}
           <Link href="/sign-up" className="underline text-black">
@@ -44,58 +23,6 @@ const SignInPage = () => {
           </Link>
         </p>
         <div className="space-y-2">
-          <button
-            className="relative cursor-pointer flex items-center justify-center w-full font-medium py-2.5 border-2 border-zinc-300 rounded-lg bg-black text-white"
-            onClick={() => {
-              socialSignIn("github")
-              setIsGithubAuthenticating(false)
-            }}
-          >
-            <GithubIcon
-              className="absolute top-1/2 -translate-y-1/2 left-4"
-              width={25}
-              height={25}
-            />
-            <p className="">Sign in with GitHub</p>
-            {isGithubAuthenticating && (
-                          <LoaderCircle
-                            className="mx-auto animate-spin absolute top-1/2 -translate-y-1/2 right-4 text-gray-500"
-                            width={25}
-                            height={25}
-                            strokeWidth={3}
-                          />
-                        )}
-          </button>
-          <button
-            className="relative flex cursor-pointer items-center justify-center w-full font-medium py-2.5 border-2 border-zinc-300 rounded-lg"
-            onClick={() => {
-              socialSignIn("google")
-              setIsGoogleAuthenticating(true)
-            }}
-          >
-            <Image
-              className="absolute top-1/2 -translate-y-1/2 left-4"
-              src="/google.png"
-              alt="Google Sign In"
-              width={25}
-              height={25}
-            />
-            <p className="">Sign in with Google</p>
-            {isGoogleAuthenticating && (
-                          <LoaderCircle
-                            className="mx-auto animate-spin absolute top-1/2 -translate-y-1/2 right-4 text-gray-500"
-                            width={25}
-                            height={25}
-                            strokeWidth={3}
-                          />
-                        )}
-          </button>
-
-          <div className="flex items-center my-3">
-            <div className="h-px flex-1 bg-zinc-300" />
-            <p className="px-2 text-zinc-400">or continue with</p>
-            <div className="h-px flex-1 bg-zinc-300" />
-          </div>
           <SignInClient />
         </div>
       </div>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,26 +1,9 @@
 "use client";
-import { signIn } from "@/lib/auth-client";
-import { ArrowLeft, GithubIcon, LoaderCircle } from "lucide-react";
-import Image from "next/image";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import SignUpClient from "./SignUpClient";
-import { useState } from "react";
-
-
 
 const SignUpPage = () => {
-  const [isGithubAuthenticating, setIsGithubAuthenticating] = useState(false);
-  const [isGoogleAuthenticating, setIsGoogleAuthenticating] = useState(false);
-  const socialSignIn = async (provider: string) => {
-    try {await signIn.social({
-      provider,
-      callbackURL: "/dashboard",
-    })} catch (error) {
-      setIsGithubAuthenticating(false);
-      setIsGoogleAuthenticating(false);
-    }
-  };
-
   return (
     <div className="flex items-center justify-center min-h-screen">
       <Link
@@ -38,58 +21,6 @@ const SignUpPage = () => {
           </Link>
         </p>
         <div className="space-y-2">
-          <button
-            onClick={() => {
-              socialSignIn("github");
-              setIsGithubAuthenticating(true)
-            }}
-            className="relative flex items-center justify-center w-full font-medium py-2.5 border-2 border-zinc-300 rounded-lg bg-black text-white cursor-pointer"
-          >
-            <GithubIcon
-              className="absolute top-1/2 -translate-y-1/2 left-4"
-              width={25}
-              height={25}
-            />
-            <p className="">Sign up with GitHub</p>
-            {isGithubAuthenticating && (
-              <LoaderCircle
-                className="mx-auto animate-spin absolute top-1/2 -translate-y-1/2 right-4 text-gray-500"
-                width={25}
-                height={25}
-                strokeWidth={3}
-              />
-            )}
-          </button>
-          <button
-            onClick={() => {
-              socialSignIn("google");
-              setIsGoogleAuthenticating(false)
-            }}
-            className="relative flex items-center justify-center w-full font-medium py-2.5 border-2 border-zinc-300 rounded-lg cursor-pointer"
-          >
-            <Image
-              className="absolute top-1/2 -translate-y-1/2 left-4"
-              src="/google.png"
-              alt="Google Sign In"
-              width={25}
-              height={25}
-            />
-            <p className="">Sign up with Google</p>
-            {isGoogleAuthenticating && (
-              <LoaderCircle
-                className="mx-auto animate-spin absolute top-1/2 -translate-y-1/2 right-4 text-gray-500"
-                width={25}
-                height={25}
-                strokeWidth={3}
-              />
-            )}
-          </button>
-
-          <div className="flex items-center my-3">
-            <div className="h-px flex-1 bg-zinc-300" />
-            <p className="px-2 text-zinc-400">or continue with</p>
-            <div className="h-px flex-1 bg-zinc-300" />
-          </div>
           <SignUpClient />
         </div>
       </div>
