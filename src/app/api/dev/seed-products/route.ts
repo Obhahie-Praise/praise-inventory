2f5569bcd87dbda78fa389023632663f867ea40d
Obhahie Praise <obhahiepraise@gmail.com>
2026-01-06 14:01:24 +0100

    fix deployment error

--- src/app/api/dev/seed-products/route.ts
+++ src/app/api/dev/seed-products/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function POST() {
   const demoUserId = "OadkXpMIVEBltkT0SqJN5HYaUWcbyTFX";
