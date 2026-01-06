0203f1680fc0e9340a16249044b93bf01b1052b1
Obhahie Praise <obhahiepraise@gmail.com>
2026-01-06 14:26:24 +0100

    fix deployment error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
 const prisma =
-  globalForPrisma.prisma || new PrismaClient({
+  globalForPrisma.prisma ||
+  new PrismaClient({
     adapter,
   });
 
